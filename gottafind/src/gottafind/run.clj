8593d3c53067f537fc0a1cfbfda841e5fc6a4a7f
Paul deGrandis <paul.degrandis@gmail.com>
2011-04-19 00:43:21 -0700

    All server launching is isolated via run.clj now

--- gottafind/src/gottafind/run.clj
+++ gottafind/src/gottafind/run.clj
@@ -1,11 +1,38 @@
-(ns gottafind.run)
+(ns ^{:doc "Used primarily for running/launching the severs"
+      :author "Paul deGrandis"}
+  gottafind.run
+  (:require [gottafind.core :as gfc]
+            [aleph.http :as aleph]))
 
-(defn start-http []
-  )
+(def ws-default-port 8888)
+(def http-default-port 8080)
 
-(defn start-ws []
-  )
+(defn start-http
+  "Start the http server.
+  Arguments:
+    [port] - an int, the server's port number [http-default-port :: 8080]
+  Notes:
+    THIS WILL BLOCK --
+      and is only meant for launching the server via the repl or lein-run"
+  ([]
+   (start-http http-default-port))
+  ([port]))
+
+(defn start-ws
+  "Start the websocket server.
+  Arguments:
+    [port] - an int, the server's port number [ws-default-port :: 8888]
+  Notes:
+    THIS WILL BLOCK --
+      and is only meant for launching the server via the repl or lein-run"
+  ([]
+   (start-ws ws-default-port))
+  ([port]
+   aleph/start-http-server gfc/loc-handler {:port port :websocket true}))
 
 (defn -main [& [server-type port]]
-  )
+  (condp = server-type
+    "ws"    (start-ws (or port ws-default-port))
+    "http"  (start-http (or port http-default-port))
+    (println "\nError: That server-type is not recogized")))
 

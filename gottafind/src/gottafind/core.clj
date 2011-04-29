d0dad844f73c0ba2d7bccd3daa678ec33b8032d7
Paul deGrandis <paul.degrandis@gmail.com>
2011-04-28 22:19:04 -0700

    WIP: Aleph handling all http requests, sessions, and ws pushing

--- gottafind/src/gottafind/core.clj
+++ gottafind/src/gottafind/core.clj
@@ -1,8 +1,22 @@
 (ns gottafind.core
+  (:use [net.cgrand.moustache :only (app)])
   (:require [lamina.core :as lamina]
-            [aleph.http :as aleph]))
+            [aleph.http :as aleph]
+            [gottafind.session :as session]))
 
 (def broadcast-channel (lamina/channel))
+
+(defn serve-session [resp-ch request session-id]
+  "LOOK AT MATCHLEND TO SEE HOW TO PASS THE SESSION IN")
+
+(defn init-session [resp-ch request]
+  (future (let [session-id (session/create-session!)]
+            (serve-session resp-ch request session-id))))
+
+(def web-handler
+  (app
+    [""] {:get (aleph/wrap-aleph-handler init-session)}
+    [session-key] {:get (aleph/wrap-aleph-handler serve-session)}))
 
 (defn loc-handler [ch handshake]
   (lamina/receive ch
